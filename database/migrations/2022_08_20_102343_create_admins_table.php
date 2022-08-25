4877672d80bbcfdbb7b58b33ec22a5292f1942a4
ayowande <66342315+ayowandeapp@users.noreply.github.com>
2022-08-25 11:37:59 +0100

    Add files via upload

---
+++ database/migrations/2022_08_20_102343_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('type')->default('Admin');
+            $table->tinyInteger('categories_access')->default('1');
+            $table->tinyInteger('products_access')->default('1');
+            $table->tinyInteger('orders_access')->default('1');
+            $table->tinyInteger('users_access')->default('1');
+            $table->tinyInteger('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
---
+++ database/migrations/2022_08_20_104057_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');      
+            $table->string('user_email');
+            $table->string('name');        
+            $table->string('mobile');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('pincode');
+            $table->string('coupon_code');
+            $table->integer('coupon_amount');
+            $table->string('payment_method');
+            $table->integer('grand_total');
+            $table->tinyInteger('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
